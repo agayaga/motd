458b2a6fd35b7201236eb0b6b3d44c1221961bc4
OptimusCrime <thomasgautv@hotmail.com>
2015-12-22 22:02:36 +0100

    Initial commit

---
+++ script.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python2
+# -*- coding: utf8 -*-
+
+# Import stuff we need
+import os
+import random
+
+# Open the file
+file = open('/var/scripts/motd/quotes.txt','r')
+
+# Read the entire file and append each line to the array
+quotes = []
+for line in file:
+    quotes.append(line.strip())
+file.close()
+
+# Generate random quote based on the amount of quotes we found
+num = random.randint(0, len(quotes)-1)
+
+# Clear content
+open('/etc/motd', 'w').close()
+
+# Change the motd-file
+with open('/etc/motd', 'a') as file:
+    file.write("\n" + quotes[num] + "\n")
